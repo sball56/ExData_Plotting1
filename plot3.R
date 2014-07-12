959772baf0ea490824ecf1baf8099fef992dd612
sball56 <sball56x@gmail.com>
2014-07-12 12:26:41 -0300

    added plot3

--- plot3.R
+++ plot3.R
@@ -13,11 +13,11 @@ plot3 <- function() {
     png(file = "plot3.png", width = 480, height = 480, units = "px")
     
     # plot3 
-    plot(tmpData$theDateTime, tmpData$Sub_metering_1, type="n", 
+    plot(plotData$theDateTime, plotData$Sub_metering_1, type="n", 
          xlab="", ylab="Energy sub metering")
-    lines(x=tmpData$theDateTime, tmpData$Sub_metering_1, type="l", col="black")
-    lines(x=tmpData$theDateTime, tmpData$Sub_metering_2, type="l", col="red")
-    lines(x=tmpData$theDateTime, tmpData$Sub_metering_3, type="l", col="blue")
+    lines(x=plotData$theDateTime, plotData$Sub_metering_1, type="l", col="black")
+    lines(x=plotData$theDateTime, plotData$Sub_metering_2, type="l", col="red")
+    lines(x=plotData$theDateTime, plotData$Sub_metering_3, type="l", col="blue")
     
     legend( x="topright", 
             legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
